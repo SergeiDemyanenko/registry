92aa049fbd89eea0e53506fb7dd2caa2d3d93809
Sergei Demyanenko <Sergei.Demyanenko@gmail.com>
2019-07-20 09:00:16 -0700

    Revert "[DB] refactoring"
    This reverts commit f1fb389

--- source/db/recreateDB.cmd
+++ source/db/recreateDB.cmd
@@ -1,5 +1,5 @@
 set targetDir=..\..\target\db
 set targetFile=db.fdb
-if not exist %targetDir% (mkdir %targetDir%)
-if exist %targetDir%\%targetFile% (del /F /Q %targetDir%\%targetFile%)
+if not exist %targetDir% mkdir %targetDir%
+if exist %targetDir%\%targetFile% del /F /Q %targetDir%\%targetFile%
 isql -input ddl.sql
