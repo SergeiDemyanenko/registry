34d007a97975733e5fd87e767b63a634c9574e41
Sergei Demyanenko <Sergei.Demyanenko@gmail.com>
2019-07-29 02:40:39 -0700

    [report] #JAV-43 refactoring

--- source/server/registry/src/main/java/dataBase/GetResultSet.java
+++ source/server/registry/src/main/java/dataBase/GetResultSet.java
@@ -4,6 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @FunctionalInterface
-public interface GetResultFromSet<T> {
-    T get(ResultSet resultSet) throws SQLException;
+public interface GetResultSet {
+    void get(ResultSet resultSet) throws SQLException;
 }
--- source/server/registry/src/main/java/report/Report.java
+++ source/server/registry/src/main/java/report/Report.java
@@ -10,33 +10,47 @@ import java.sql.SQLException;
 public class Report {
 
     private static final String GET_REPORT_BODY_SQL =
-            " select R.ID_REPORTS_HTML, R.SQL, R.TEXT, R.PARENT_ID" +
+            " select R.ID_REPORTS_HTML as ID, R.SQL, R.TEXT, R.PARENT_ID" +
             " from APP$REPORTS_HTML R" +
             " where R.REPORTS_ID = %1$d and %2$d = coalesce(R.PARENT_ID, 0)" +
             " order by R.INDEX_";
+
+    private static Object[] getArrayFromRow(ResultSet resultSet) throws SQLException {
+        Object[] result = new Object[resultSet.getMetaData().getColumnCount()];
+        for (int i = 1; i <= result.length; i++) {
+            result[i - 1] = resultSet.getObject(i);
+        }
+        return result;
+    }
 
     private static void appendRow(Connection connection, long id, long root, ResultSet data, StringBuilder resultBuilder) throws SQLException {
 
         DataBase.getResultFromSQL(connection, String.format(GET_REPORT_BODY_SQL, id, root), reportSet -> {
             while (reportSet.next()) {
-                DataBase.getResultFromSQL(connection, reportSet.getString("SQL"),
-                        // TODO Fix next line, parameterMetaData.getParameterTypeName(i) - the code should return name of parameter
-                        (ParameterMetaData parameterMetaData, int i) -> data.getObject(parameterMetaData.getParameterTypeName(i)),
-                        dataSet -> {
-                            while (dataSet.next()) {
-                                Object[] parameterArray = new Object[dataSet.getMetaData().getColumnCount()];
-                                for (int i = 0; i < parameterArray.length; i++) {
-                                    parameterArray[i] = dataSet.getObject(i);
-                                }
+                String itemSql = reportSet.getString("SQL");
+                String itemText = reportSet.getString("TEXT");
+                Long itemRoot = reportSet.getLong("ID");
 
-                                resultBuilder.append(String.format(reportSet.getString("TEXT"), parameterArray));
-                                appendRow(connection, id, reportSet.getLong("PARENT_ID"), dataSet, resultBuilder);
+                if (itemSql != null && !itemSql.isEmpty()) {
+                    DataBase.getResultFromSQL(connection, itemSql,
+                            // TODO Fix next line, parameterMetaData.getParameterTypeName(i) - the code should return name of parameter
+                            (ParameterMetaData parameterMetaData, int i) -> data.getObject(parameterMetaData.getParameterTypeName(i)),
+                            itemData -> {
+                                while (itemData.next()) {
+                                    if (itemText != null && !itemText.isEmpty()) {
+                                        resultBuilder.append(String.format(itemText.replace(":s", "$s"), getArrayFromRow(itemData)));
+                                    }
+                                    appendRow(connection, id, itemRoot, itemData, resultBuilder);
+                                }
                             }
-                            return null;
-                        }
-                );
+                    );
+                } else {
+                    String text = reportSet.getString("TEXT");
+                    if (text != null && !text.isEmpty()) {
+                        resultBuilder.append(text);
+                    }
+                }
             }
-            return null;
         });
     }
 
