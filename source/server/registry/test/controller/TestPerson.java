b77b684ad5a2b9a0280f1427f8e487da4e52513f
maxym <maksym.iurchenko@gmail.com>
2019-08-02 14:35:53 -0700

    test for person done

--- source/server/registry/test/controller/TestPerson.java
+++ source/server/registry/test/controller/TestPerson.java
@@ -1,20 +1,48 @@
 package controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.sql.Connection;
+import java.sql.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class TestPerson {
 
+    @InjectMocks
     private DataBase dataBase = mock(DataBase.class);
+    @Mock
+    private Connection mockConnection;
+    @Mock
+    private PreparedStatement mockStatement;
+    @Mock
+    private ResultSet rs;
+    @Mock
+    private ResultSetMetaData rsmd;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
-    public void testPerson() {
-        Connection connection = dataBase.getConnection();
-        when(dataBase.getJsonFromSQL(connection, "SELECT  * from PERSON")).thenReturn("");
+    public void testPerson() throws SQLException {
+        when(mockConnection.prepareStatement("SELECT  * from PERSON")).thenReturn(mockStatement);
+        when(mockStatement.getResultSet()).thenReturn(rs);
+        when(mockStatement.executeQuery()).thenReturn(rs);
+        when(rs.getMetaData()).thenReturn(rsmd);
+        when(rsmd.getColumnCount()).thenReturn(3);
+        when(rs.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(rsmd.getCatalogName(1)).thenReturn("Person1");
+        when(rsmd.getCatalogName(2)).thenReturn("Person2");
+        when(rsmd.getCatalogName(3)).thenReturn("Person3");
+        //when(dataBase.getJsonFromSQL(mockConnection, "SELECT  * from PERSON")).thenReturn("list of users provided");
+
+        assertEquals(dataBase.getJsonFromSQL(mockConnection, "SELECT  * from PERSON"), "[{},{}]");
 
     }
 
