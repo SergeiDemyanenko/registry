ad0c0d7a1bcf2151919b1023299ce8fc97898105
Anastasia Goygova <goygova@gmail.com>
2019-08-19 22:48:58 -0700

    JAV-60 Adjust table view according to API response data

--- source/client/registry-client/src/styles/Person.styles.js
+++ source/client/registry-client/src/styles/Person.styles.js
@@ -2,5 +2,12 @@ export default (theme) => ({
     tableContainer: {
         height: 'calc(100vh - 140px)',
         overflow: 'scroll',
+    },
+    loadingIndicatorContainer: {
+        height: 'inherit',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
     }
 })
