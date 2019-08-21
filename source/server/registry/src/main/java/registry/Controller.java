10f0ab0b6b248a2e4669e3574f6c7cd50979fde6
sergey_demyanenko <sergei.demyanenko@gmail.com>
2019-08-21 12:29:26 -0700

    [back-end] #JAV-65 added end-point for menu

--- source/server/registry/src/main/java/registry/Controller.java
+++ source/server/registry/src/main/java/registry/Controller.java
@@ -1,5 +1,7 @@
 package registry;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import registry.dataBase.DataBase;
@@ -12,7 +14,8 @@ import registry.report.Report;
 
 import javax.sql.DataSource;
 import java.nio.charset.StandardCharsets;
-import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -31,6 +34,42 @@ public class Controller {
     @RequestMapping("/api/hi")
     public String hi() {
         return "hi";
+    }
+
+    private Map<String, String> menuMap = new HashMap<>();
+    {
+        menuMap.put("Физические лица", "api/directory/person");
+        menuMap.put("Статус заявки", "api/directory/appStatus");
+        menuMap.put("Характеристики", "api/directory/characteristics");
+        menuMap.put("Результат проверки", "api/directory/checkResult");
+        menuMap.put("Орган СРО", "api/directory/department");
+        menuMap.put("Основания для исключения", "api/directory/expulsion");
+        menuMap.put("Компенсационный фонд ОДО", "api/directory/findSumContact");
+        menuMap.put("Компенсационный фонд ВВ", "api/directory/findSumHarm");
+    }
+
+    private static final String TITLE = "TITLE";
+    private static final String ITEMS = "ITEMS";
+    private static final String LINK = "LINK";
+    private static final String ORDER = "ORDER";
+
+    @RequestMapping ("/api/menu")
+    public String menu () {
+        JSONArray result = new JSONArray();
+
+        // dict
+        JSONArray dictMenu = new JSONArray();
+        for (Map.Entry<String, String> entry : menuMap.entrySet()) {
+            dictMenu.put(new JSONObject().put(TITLE, entry.getKey()).put(ORDER, dictMenu.length()).put(LINK, entry.getValue()));
+        }
+        result.put(new JSONObject().put(TITLE, "Справочники").put(ITEMS, dictMenu).put(ORDER, result.length()));
+
+        result.put(new JSONObject().put(TITLE, "Документы").put(ORDER, result.length()).put(ITEMS, new JSONObject().put(TITLE, "test_line")));
+        result.put(new JSONObject().put(TITLE, "Отчеты").put(ORDER, result.length()).put(ITEMS, new JSONObject().put(TITLE, "test_line")));
+        result.put(new JSONObject().put(TITLE, "Настройки").put(ORDER, result.length()).put(ITEMS, new JSONObject().put(TITLE, "test_line")));
+        result.put(new JSONObject().put(TITLE, "Помощь").put(ORDER, result.length()).put(ITEMS, new JSONObject().put(TITLE, "test_line")));
+
+        return new JSONObject().put(ITEMS, result).toString();
     }
 
     @RequestMapping ("/api/directory/person")
