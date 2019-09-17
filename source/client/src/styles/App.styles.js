73c81aa4689e91148c290cdbcabdeefb79e50b69
Anastasia Goygova <goygova@gmail.com>
2019-09-17 16:34:05 -0700

    jav-78 Move client code one folder up

---
+++ source/client/src/styles/App.styles.js
@@ -0,0 +1,46 @@
+const drawerWidth = 240;
+
+export default theme => ({
+	root: {
+		display: 'flex'
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0
+		}
+	},
+	appBar: {
+		marginLeft: drawerWidth,
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`
+		}
+	},
+	menuButton: {
+		color: 'inherit',
+		ariaLabel: 'Open drawer',
+		edge: 'start',
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none'
+		}
+	},
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
+		width: drawerWidth
+	},
+	content: {
+		flexGrow: 1,
+		paddingLeft: theme.spacing(3),
+		paddingRight: theme.spacing(3),
+		width: 'calc(100vw - 288px)'
+	},
+	nested: {
+		paddingLeft: theme.spacing(3)
+	},
+
+	link: {
+		color: 'inherit',
+		textDecoration: 'none'
+	}
+});
