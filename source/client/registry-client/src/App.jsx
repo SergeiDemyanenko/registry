b63f523ea65c2d800b5a0504f8aa1b162ed336d8
Anastasia Goygova <goygova@gmail.com>
2019-09-17 10:36:16 -0700

    jav-77 Create a recursive Menu

---
+++ source/client/registry-client/src/App.jsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import appStyles from './styles/App.styles';
+import { Route, Link } from 'react-router-dom';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import axios from 'axios';
+import Person from './components/directory/Person';
+
+class App extends React.Component {
+	componentWillMount() {
+		this.fetchMenu().then(response => {
+			this.props.setMenu(response.data);
+		});
+	}
+
+	fetchMenu() {
+		return axios({
+			method: 'get',
+			url: '/api/menu',
+			responseType: 'json'
+		});
+	}
+
+	buildMenu(leftMenuItems) {
+		return (
+			<List>
+				{leftMenuItems.map(leftMenuItem => {
+					if (leftMenuItem.items) {
+						return (
+							<div key={leftMenuItem.title}>
+								<ListItem
+									button
+									key={leftMenuItem.title}
+									onClick={() => {
+										this.props.toggleLeftMenuItem(leftMenuItem.name);
+									}}
+									selected={this.props.location.pathname === leftMenuItem.link}>
+									{/* TODO: Ask about icons */}
+									{/* <ListItemIcon>{leftMenuItem.icon}</ListItemIcon> */}
+									<ListItemText primary={leftMenuItem.title} />
+									{leftMenuItem.isExpanded ? <ExpandLess /> : <ExpandMore />}
+								</ListItem>
+								<Collapse in={leftMenuItem.isExpanded} timeout='auto' unmountOnExit className={this.props.classes.nested}>
+									{this.buildMenu(leftMenuItem.items)}
+								</Collapse>
+							</div>
+						);
+					} else {
+						return (
+							<Link to={leftMenuItem.path} className={this.props.classes.link}>
+								<ListItem button key={leftMenuItem.title}>
+									<ListItemText primary={leftMenuItem.title} />
+								</ListItem>
+							</Link>
+						);
+					}
+				})}
+			</List>
+		);
+	}
+
+	getDrawer() {
+		return (
+			<div>
+				<div className={this.props.classes.toolbar} />
+				<Divider />
+				{this.buildMenu(this.props.app.leftMenuItems)}
+			</div>
+		);
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.root}>
+				<AppBar position='fixed' className={classes.appBar}>
+					<Toolbar>
+						<IconButton onClick={this.props.toggleLeftMenu} className={classes.menuButton}>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant='h6' noWrap>
+							Registry Client
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<nav className={classes.drawer} aria-label='Mailbox folders'>
+					{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+					<Hidden smUp implementation='css'>
+						<Drawer
+							variant='temporary'
+							// anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+							open={this.props.app.leftMenuOpen}
+							onClick={this.props.toggleLeftMenu}
+							classes={{
+								paper: classes.drawerPaper
+							}}
+							ModalProps={{
+								keepMounted: true // Better open performance on mobile.
+							}}>
+							{this.getDrawer()}
+						</Drawer>
+					</Hidden>
+					<Hidden xsDown implementation='css'>
+						<Drawer
+							classes={{
+								paper: classes.drawerPaper
+							}}
+							variant='permanent'
+							open>
+							{this.getDrawer()}
+						</Drawer>
+					</Hidden>
+				</nav>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					<Route path='/directory/person' component={Person} />
+				</main>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		app: state.appReducer
+	};
+};
+const mapDispatchToProps = dispatch => {
+	return {
+		setName: name => {
+			dispatch({
+				type: 'SET_NAME',
+				payload: name
+			});
+		},
+		toggleLeftMenu: () => {
+			dispatch({
+				type: 'TOGGLE_LEFT_MENU'
+			});
+		},
+		toggleLeftMenuItem: name => {
+			dispatch({
+				type: 'TOGGLE_LEFT_MENU_ITEM',
+				payload: name
+			});
+		},
+		setMenu: menu => {
+			dispatch({
+				type: 'SET_MENU',
+				payload: menu
+			});
+		}
+	};
+};
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withStyles(appStyles)(App));
--- source/client/registry-client/src/reducers/appReducer.js
+++ source/client/registry-client/src/reducers/appReducer.js
@@ -1,41 +1,56 @@
 const initialAppState = {
-    leftMenuOpen: false,
-    leftMenuItems: []
-}
+	leftMenuOpen: false,
+	leftMenuItems: []
+};
 
 export default (state = initialAppState, action) => {
-    switch(action.type){
-        case 'TOGGLE_LEFT_MENU':
-           state = { 
-               ...state,
-               leftMenuOpen: !state.leftMenuOpen
-           };
-           break;
-        case 'TOGGLE_LEFT_MENU_ITEM':
-            state = {
-                ...state,
-                leftMenuItems: state.leftMenuItems.map((item, index) => {
-                    if(index !== action.payload) {
-                        return {
-                            ...item,
-                            isExpanded: false
-                        }
-                    }
-                    return {
-                        ...item,
-                        isExpanded: !item.isExpanded
-                    }
-                })
-            }
-            break;
-        case 'SET_MENU':
-            state = {
-                ...state,
-                leftMenuItems: action.payload.items
-            }
-            break;
-        default:
-        return state;
-    }
-    return state;
+	switch (action.type) {
+		case 'TOGGLE_LEFT_MENU':
+			state = {
+				...state,
+				leftMenuOpen: !state.leftMenuOpen
+			};
+			break;
+		case 'TOGGLE_LEFT_MENU_ITEM':
+			state = {
+				...state,
+				leftMenuItems: toggleLeftMenuItemByName(action.payload, state.leftMenuItems)
+			};
+			break;
+		case 'SET_MENU':
+			state = {
+				...state,
+				leftMenuItems: action.payload
+			};
+			break;
+		default:
+			return state;
+	}
+	return state;
+};
+
+/**
+ * Toggles left menu item by its name
+ * @param {*} name
+ * @param {*} state
+ * @returns {*} leftMenuItems with toggled isExpanded propertie for matched item by name
+ */
+function toggleLeftMenuItemByName(name, leftMenuItems) {
+	return leftMenuItems.map(leftMenuItem => {
+		if (leftMenuItem.name === name) {
+			return {
+				...leftMenuItem,
+				isExpanded: !leftMenuItem.isExpanded
+			};
+		} else if (leftMenuItem.items) {
+			return {
+				...leftMenuItem,
+				items: toggleLeftMenuItemByName(name, leftMenuItem.items)
+			};
+		}
+		return {
+			...leftMenuItem,
+			isExpanded: false
+		};
+	});
 }
