9ebcbd8d9f2388ed2be9630d0cbace36650cf9ff
Anastasia Goygova <goygova@gmail.com>
2019-10-16 22:26:06 -0700

    Modified code to work with models

--- source/client/src/store.js
+++ source/client/src/store.js
@@ -1,9 +1,9 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
 import appReducer from './reducers/appReducer';
-import directoryReducer from './reducers/directoryReducer';
+import dataReducer from './reducers/dataReducer';
 
 export default createStore(
-    combineReducers({appReducer, directoryReducer}), 
-    //checks if Redux devtools are installed in browser
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	combineReducers({ appReducer, dataReducer }),
+	//checks if Redux devtools are installed in browser
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
