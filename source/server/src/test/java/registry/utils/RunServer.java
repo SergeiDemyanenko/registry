621c488fcb86644de1b64c93ea6b4d5fcafe750d
TSSoftware-hub <sergey.trukhanov@outlook.com>
2020-03-05 20:38:36 -0800

    REG-9: added test for /api/hi, deleted all old tests.

---
+++ source/server/src/test/java/registry/utils/RunServer.java
@@ -0,0 +1,20 @@
+package registry.utils;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import registry.Application;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        classes = Application.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public @interface RunServer {
+}
